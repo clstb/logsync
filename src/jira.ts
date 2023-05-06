f06439e7975264bc1e6fa0c9a440b6f830adbee7
clstb <claas.stoertenbecker@gmail.com>
2023-05-06 10:44:27 +0200

    feat(jira.ts): add support for fetching issues from Jira and inserting them as Logseq blocks in a dedicated page
    This commit adds the following features to the Jira class:
    - Fetching all boards from Jira
    - Fetching all issues for the active sprint of a specific board
    - Parsing the fetched issues into a custom Issue type
    - Inserting the fetched issues as Logseq blocks in a dedicated page named "jira/issues"
    
    The commit also adds the following helper functions:
    - `ensureIssuesPage` to ensure the existence of the "jira/issues" page
    - `jiraBoards` to fetch all boards from Jira
    - `jiraIssues` to fetch all issues for the active sprint of a specific board
    - `jiraSprint` to fetch the active sprint of a specific board
    - `parseJiraIssue` to parse a Jira issue into a custom Issue type
    - `issueToBlock` to

--- src/jira.ts
+++ src/jira.ts
@@ -1,8 +1,78 @@
+import "@logseq/libs";
+import { PageEntity } from "@logseq/libs/dist/LSPlugin.user";
 import { AgileClient } from "jira.js"
+import { Board, Issue as JiraIssue, Sprint } from "jira.js/out/agile/models"
 
-async function jiraBoards(client: AgileClient) {
-  const boards = await client.board.getAllBoards()
-  console.log(boards)
+type Issue = {
+  id: string;
+  key: string;
+  url: string;
+  summary: string;
+
+  block_uuid?: string;
+}
+
+async function ensureIssuesPage(): Promise<PageEntity> {
+  const page = await logseq.Editor.getPage("jira/issues");
+  if (!page) {
+    return await logseq.Editor.createPage("jira/issues");
+  }
+  return page;
+}
+
+async function jiraBoards(client: AgileClient): Promise<Record<number, Board>> {
+  const boards: Record<number, Board> = {}
+  const response = await client.board.getAllBoards()
+  response.values.forEach((board) => {
+    boards[board.id] = board
+  })
+  return boards
+}
+
+async function jiraIssues(client: AgileClient, sprintId: number): Promise<Record<number, Issue>> {
+  const issues: Record<number, Issue> = {}
+  // Fetch all issues for the sprint
+  const response = await client.sprint.getIssuesForSprint({
+    sprintId: sprintId,
+    startAt: 0,
+    maxResults: 100,
+  })
+  response.issues.forEach((issue) => {
+    issues[issue.id] = parseJiraIssue(issue)
+  })
+  return issues
+}
+
+function parseJiraIssue(issue: JiraIssue): Issue {
+  return {
+    id: issue.id,
+    key: issue.key,
+    url: issue.self,
+    summary: issue.fields.summary,
+  }
+}
+
+async function jiraSprint(client: AgileClient, boardId: number): Promise<Sprint> {
+  const response = await client.board.getAllSprints({
+    boardId: boardId,
+    state: "active",
+  })
+  return response.values[0]
+}
+
+function issueToBlock(issue: Issue) {
+  return {
+    uuid: issue.block_uuid,
+    content: `[${issue.key}: ${issue.summary}](${issue.url})`,
+  }
+}
+
+async function insertIssueBlocks(page: PageEntity, issues: Issue[]) {
+  const blocks = issues.map(issueToBlock)
+  if (blocks.length === 0) {
+    return
+  }
+  await logseq.Editor.insertBatchBlock(page.uuid, blocks, { before: false, sibling: true })
 }
 
 export class Jira {
@@ -19,6 +89,11 @@ export class Jira {
     })
   }
   sync = async function() {
-    await jiraBoards(this.client)
+    const page = await ensureIssuesPage()
+    const boards = await jiraBoards(this.client)
+    const board = boards[2]
+    const sprint = await jiraSprint(this.client, board.id)
+    const issues = await jiraIssues(this.client, sprint.id)
+    await insertIssueBlocks(page, Object.values(issues))
   }
 }
