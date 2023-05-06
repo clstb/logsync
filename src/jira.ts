8536b364ddc73ee1f8f2a9dc85add9d3372b3325
clstb <claas.stoertenbecker@gmail.com>
2023-05-06 11:25:09 +0200

    feat(jira.ts): add support for updating existing issues in Logseq when changes are detected in Jira, and deleting issues that are no longer present in Jira. Also, add more properties to the Issue type to better represent Jira issues.

--- src/jira.ts
+++ src/jira.ts
@@ -8,6 +8,11 @@ type Issue = {
   key: string;
   url: string;
   summary: string;
+  creator: string;
+  assignee?: string;
+  created: string;
+  updated: string;
+  status: string;
 
   block_uuid?: string;
 }
@@ -29,6 +34,30 @@ async function jiraBoards(client: AgileClient): Promise<Record<number, Board>> {
   return boards
 }
 
+async function logseqIssues(): Promise<Record<string, Issue>> {
+  const blocks = await logseq.Editor.getPageBlocksTree("/jira/issues");
+  const issues = {};
+  for (const block of blocks) {
+    if (!block.properties || !block.properties[".id"]) {
+      continue;
+    }
+    const issue = {
+      id: block.properties[".id"],
+      key: block.properties[".key"],
+      url: block.properties[".url"],
+      summary: block.properties[".summary"],
+      creator: block.properties["creator"],
+      assignee: block.properties["assignee"],
+      created: block.properties["created"],
+      updated: block.properties["updated"],
+      status: block.properties["status"],
+      block_uuid: block.uuid,
+    }
+    issues[issue.id] = issue
+  }
+  return issues;
+}
+
 async function jiraIssues(client: AgileClient, sprintId: number): Promise<Record<number, Issue>> {
   const issues: Record<number, Issue> = {}
   // Fetch all issues for the sprint
@@ -44,12 +73,34 @@ async function jiraIssues(client: AgileClient, sprintId: number): Promise<Record
 }
 
 function parseJiraIssue(issue: JiraIssue): Issue {
-  return {
+  const nthNumber = (n: number) => {
+    const s = ["th", "st", "nd", "rd"];
+    const v = n % 100;
+    return n + (s[(v - 20) % 10] || s[v] || s[0]);
+  };
+
+  const created = new Date(issue.fields.created);
+  const createdStr = `${created.toLocaleString("default", { month: "short" })} ${nthNumber(created.getDate())}, ${created.getFullYear()}`;
+  const updated = new Date(issue.fields.updated);
+  const updatedStr = `${updated.toLocaleString("default", { month: "short" })} ${nthNumber(updated.getDate())}, ${updated.getFullYear()}`;
+  const creator = `[[jira/people/${issue.fields.creator.displayName}]]`
+
+  const parsed: Issue = {
     id: issue.id,
     key: issue.key,
     url: issue.self,
     summary: issue.fields.summary,
+    created: createdStr,
+    updated: updatedStr,
+    creator: creator,
+    status: issue.fields.status.name,
   }
+
+  if (issue.fields.assignee) {
+    parsed.assignee = `[[jira/people/${issue.fields.assignee.displayName}]]`
+  }
+
+  return parsed
 }
 
 async function jiraSprint(client: AgileClient, boardId: number): Promise<Sprint> {
@@ -64,7 +115,24 @@ function issueToBlock(issue: Issue) {
   return {
     uuid: issue.block_uuid,
     content: `[${issue.key}: ${issue.summary}](${issue.url})`,
+    properties: {
+      ".id": issue.id,
+      ".key": issue.key,
+      ".url": issue.url,
+      ".summary": issue.summary,
+      "page": `[[jira/issues/${issue.key}]]`,
+      "creator": issue.creator,
+      "assignee": issue.assignee,
+      "created": `[[${issue.created}]]`,
+      "updated": `[[${issue.updated}]]`,
+      "status": issue.status,
+    }
   }
+}
+
+async function updateIssueBlock(issue: Issue) {
+  const block = issueToBlock(issue)
+  await logseq.Editor.updateBlock(issue.block_uuid, block.content, { properties: block.properties })
 }
 
 async function insertIssueBlocks(page: PageEntity, issues: Issue[]) {
@@ -73,6 +141,34 @@ async function insertIssueBlocks(page: PageEntity, issues: Issue[]) {
     return
   }
   await logseq.Editor.insertBatchBlock(page.uuid, blocks, { before: false, sibling: true })
+}
+
+function updateIssue(local: Issue, remote: Issue): [Issue, boolean] {
+  let changed = false;
+  if (local.summary !== remote.summary) {
+    local.summary = remote.summary;
+    changed = true;
+  }
+  if (local.assignee !== remote.assignee) {
+    local.assignee = remote.assignee;
+    changed = true;
+  }
+  if (local.status !== remote.status) {
+    local.status = remote.status;
+    changed = true;
+  }
+  if (local.updated !== remote.updated) {
+    local.updated = remote.updated;
+    changed = true;
+  }
+
+  return [local, changed];
+}
+
+async function deleteIssueBlocks(issues: Issue[]) {
+  for (const issue of issues) {
+    await logseq.Editor.removeBlock(issue.block_uuid);
+  }
 }
 
 export class Jira {
@@ -93,7 +189,28 @@ export class Jira {
     const boards = await jiraBoards(this.client)
     const board = boards[2]
     const sprint = await jiraSprint(this.client, board.id)
-    const issues = await jiraIssues(this.client, sprint.id)
-    await insertIssueBlocks(page, Object.values(issues))
+    const local = await logseqIssues()
+    const remote = await jiraIssues(this.client, sprint.id)
+
+    const toInsert = [];
+    const toDelete = [];
+    for (const issue of Object.values(remote)) {
+      if (local[issue.id]) {
+        const [updated, changed] = updateIssue(local[issue.id], issue)
+        if (changed) {
+          await updateIssueBlock(updated)
+        }
+      } else {
+        toInsert.push(issue)
+      }
+    }
+    for (const issue of Object.values(local)) {
+      if (!remote[issue.id]) {
+        toDelete.push(issue)
+      }
+    }
+
+    await insertIssueBlocks(page, toInsert);
+    await deleteIssueBlocks(toDelete);
   }
 }
