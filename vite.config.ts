1e3c5b4e95aedc8197d63253206a1cd53a442591
clstb <claas.stoertenbecker@gmail.com>
2023-12-18 00:34:16 +0100

    feat: update project meta files, add linter
    Signed-off-by: clstb <claas.stoertenbecker@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
+import {defineConfig} from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: './',
   build: {
+    outDir: 'build',
     sourcemap: true,
-  }
+  },
 });
